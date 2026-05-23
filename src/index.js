d6faaa854a532e3e7c47c8d3dafd822193304116
Geilson Almeida Barbosa <chuqsilva@gmail.com>
2026-05-23 20:23:11 -0300

    Primeira commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4, 
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+};
+
+
+
+const player4 = {
+    NOME: "Yoshi",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 4,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+const player3 = {
+    NOME: "Bowser",
+    VELOCIDADE: 5,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+};
+
+
+const player5 = {
+    NOME: "DONKEY KONG",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+};
+
+
+
+function rollDice(){
+    return Math.floor(Math.random() * 6) + 1;
+    
+}
