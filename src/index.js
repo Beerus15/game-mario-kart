97d1e5f11d3ea280dd8e1ea30d71494b44e1f011
Geilson Almeida Barbosa <chuqsilva@gmail.com>
2026-05-23 22:09:41 -0300

    Versão 4.3.0 grande atualização: criação de função das retas, curvas e confrontos

--- src/index.js
+++ src/index.js
@@ -48,6 +48,38 @@ async function rollDice(){
     
 }
 
+
+async function getRandomBlock() {
+    let random = Math.random();
+    let result
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;
+        default:
+            result = "CONFRONTO";   
+    } 
+    return result;       
+}
+
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round <= 5; round++){
+        console.log(`🏁 Rodada ${round}`);
+    // sortear bloco
+    let block = await getRandomBlock();
+    console.log(`Bloco: ${block}`);
+    }   
+}
+
 (async function main() {
     console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`);
+
+
+
+await playRaceEngine(player1, player2);
 })();
+
