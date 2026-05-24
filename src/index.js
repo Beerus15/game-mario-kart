9a85db16b35d3c5ab83b3c511373c8cd6141775b
Geilson Almeida Barbosa <chuqsilva@gmail.com>
2026-05-23 21:11:22 -0300

    Versão 3.0 grande atualização: criação de funções assyncronas

--- src/index.js
+++ src/index.js
@@ -43,7 +43,11 @@ const player5 = {
 
 
 
-function rollDice(){
+async function rollDice(){
     return Math.floor(Math.random() * 6) + 1;
     
 }
+
+(async function main() {
+    console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`);
+})();
